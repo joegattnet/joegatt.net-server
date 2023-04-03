e0c2c9254a5e62aca497e747536b2d648252aba3
Joe Gatt <joe@joegatt.net>
2023-04-03 11:06:36 +0200

    Test changes

--- aphroconfuso-daily-tasks.sh
+++ aphroconfuso-daily-tasks.sh
@@ -1,5 +1,12 @@
 echo "running..."
 
-export "$(grep -vE "^(#.*|\s*)$" .env)"
+# import variables
+eval "$(
+  cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    echo "export $key=\"$value\""
+  done
+)"
 
 echo $LISTMONK_USER
