7a5c00d813874907a92216e7b304b39080b6f65c
Joe Gatt <joe@joegatt.net>
2023-04-03 11:55:01 +0200

    Fix file destinations

--- aphroconfuso-daily-tasks.sh
+++ aphroconfuso-daily-tasks.sh
@@ -10,8 +10,8 @@ eval "$(
 )"
 
 # Backup databases
-docker exec -i postgreslistmonk /usr/bin/pg_dump -p 9433 -U $LISTMONK_USER $LISTMONK_DB | gzip -9 > ./aphroconfuo.mt-backups/postgres-listmonk-backup.sql.gz
-gpg -c --passphrase $GPG_PASSPHRASE ./aphroconfuo.mt-backups/postgres-listmonk-backup.sql.gz
+docker exec -i postgreslistmonk /usr/bin/pg_dump -p 9433 -U $LISTMONK_USER $LISTMONK_DB | gzip -9 > ~/aphroconfuso.mt-backups/postgres-listmonk-backup.sql.gz
+gpg -c --passphrase $GPG_PASSPHRASE ~/aphroconfuso.mt-backups/postgres-listmonk-backup.sql.gz
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa_deploykey_unsigned
