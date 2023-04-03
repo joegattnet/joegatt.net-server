3f6b43441cebdfcb7fbc72b5451f3bac551d2183
Joe Gatt <joe@joegatt.net>
2023-04-03 15:26:57 +0200

    Update backup script

--- aphroconfuso-daily-tasks.sh
+++ aphroconfuso-daily-tasks.sh
@@ -1,6 +1,11 @@
-echo "Running 004"
+echo "Running backups..."
 
-# import variables
+# Add to crontab
+#
+# crontab -e
+# 0 5 * * * ~/aphroconfuso-daily-tasks.sh
+
+# Import variables
 eval "$(
   cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
     key=$(echo "$line" | cut -d '=' -f 1)
@@ -15,11 +20,16 @@ cd aphroconfuso.mt-backups
 docker exec -i postgreslistmonk /usr/bin/pg_dump -p 9433 -U $LISTMONK_USER $LISTMONK_DB | gzip -9 > postgres-listmonk-backup.sql.gz
 gpg -c --passphrase $GPG_PASSPHRASE --batch --quiet postgres-listmonk-backup.sql.gz
 
-docker exec -i postgresstrapi /usr/bin/pg_dump -p 9433 -U $STRAPI_DB_USERNAME $STRAPI_DB | gzip -9 > postgres-strapi-backup.sql.gz
+docker exec -i postgresstrapi /usr/bin/pg_dump -p 5437 -U $STRAPI_DB_USERNAME $STRAPI_DB | gzip -9 > postgres-strapi-backup.sql.gz
 gpg -c --passphrase $GPG_PASSPHRASE --batch --quiet postgres-strapi-backup.sql.gz
 
+docker exec mariadbmatomo /usr/bin/mysqldump -u root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE | gzip -9 > postgres-matomo-backup.sql.gz
+gpg -c --passphrase $GPG_PASSPHRASE --batch --quiet postgres-matomo-backup.sql.gz
+
+# Delete plaintext files
 rm *-backup.sql.gz
 
+# Commit and push
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa_deploykey_unsigned
 
