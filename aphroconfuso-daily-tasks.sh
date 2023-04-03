ccd18f17fedbcd2b9928a5ee27b92c64733af705
Joe Gatt <joe@joegatt.net>
2023-04-03 11:59:20 +0200

    Test changes

--- aphroconfuso-daily-tasks.sh
+++ aphroconfuso-daily-tasks.sh
@@ -9,9 +9,12 @@ eval "$(
   done
 )"
 
+
+cd aphroconfuso.mt-backups
+
 # Backup databases
-docker exec -i postgreslistmonk /usr/bin/pg_dump -p 9433 -U $LISTMONK_USER $LISTMONK_DB | gzip -9 > ~/aphroconfuso.mt-backups/postgres-listmonk-backup.sql.gz
-gpg -c --passphrase $GPG_PASSPHRASE ~/aphroconfuso.mt-backups/postgres-listmonk-backup.sql.gz
+docker exec -i postgreslistmonk /usr/bin/pg_dump -p 9433 -U $LISTMONK_USER $LISTMONK_DB | gzip -9 > postgres-listmonk-backup.sql.gz
+gpg -c --passphrase $GPG_PASSPHRASE postgres-listmonk-backup.sql.gz
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa_deploykey_unsigned
